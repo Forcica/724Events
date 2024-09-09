4bf0e5772484c3a8614cbfea1d219a9f62dc4b6f
Forcica <47173413+Forcica@users.noreply.github.com>
2024-09-09 10:16:46 +0200

    [ADD] Add modal close on outside click and implement validation tests
    New functionality of closing the modal when clicking outside.
    The addition of tests to verify proper functioning.

--- src/containers/Modal/index.js
+++ src/containers/Modal/index.js
@@ -1,39 +1,54 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react"; // Ajout useRef et useEffect
 import Icon from "../../components/Icon";
 import "./style.scss";
 
 const Modal = ({ opened, Content, children }) => {
-  const [isOpened, setIsOpened] = useState(opened);
-  return (
-    <>
-      {children({ isOpened, setIsOpened })}
-      {isOpened && (
-        <div className="modal">
-          <div className="content">
-            {Content}
-            <button
-              type="button"
-              data-testid="close-modal"
-              onClick={() => setIsOpened(false)}
-            >
-              <Icon name="close" />
-            </button>
-          </div>
-        </div>
-      )}
-    </>
-  );
+   const [isOpened, setIsOpened] = useState(opened);
+   const modalRef = useRef(null); // Ajout pour la fonction handleClickOutside
+
+   useEffect(() => {
+      const handleClickOutside = (event) => {
+         if (modalRef.current && !modalRef.current.contains(event.target)) {
+         setIsOpened(false);
+         }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+         document.removeEventListener("mousedown", handleClickOutside);
+      };
+   }, []); // Ajout du useEffect et gestion quand on clique en dehors de la modal alors fermeture de celle ci
+
+   return (
+      <>
+         {children({ isOpened, setIsOpened })}
+         {isOpened && (
+         <div className="modal">
+            <div className="content" ref={modalRef}>
+               {Content}
+               <button
+               type="button"
+               data-testid="close-modal"
+               onClick={() => setIsOpened(false)}
+               >
+               <Icon name="close" />
+               </button>
+            </div>
+         </div>
+         )}
+      </>
+   );
 };
 
 Modal.defaultProps = {
-  opened: false,
+   opened: false,
 }
 
 Modal.propTypes = {
-  opened: PropTypes.bool,
-  Content: PropTypes.node.isRequired,
-  children: PropTypes.func.isRequired,
+   opened: PropTypes.bool,
+   Content: PropTypes.node.isRequired,
+   children: PropTypes.func.isRequired,
 }
 
 export default Modal;
