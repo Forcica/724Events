650f99a18efb6facff7b8eee79a5c026d4130ac3
Forcica <47173413+Forcica@users.noreply.github.com>
2024-10-02 12:36:01 +0200

    [ADD] New fields in the Form

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -7,50 +7,50 @@ import Button, { BUTTON_TYPES } from "../../components/Button";
 const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-        onSuccess(); // Ajout simplement de "onSuccess" car il n'y en avait pas en cas de succès de l'envoie du formulaire de contact
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
+   const [sending, setSending] = useState(false);
+   const sendContact = useCallback(
+      async (evt) => {
+         evt.preventDefault();
+         setSending(true);
+         // We try to call mockContactApi
+         try {
+         await mockContactApi();
+         setSending(false);
+         onSuccess(); // Ajout simplement de "onSuccess" car il n'y en avait pas en cas de succès de l'envoie du formulaire de contact
+         } catch (err) {
+         setSending(false);
+         onError(err);
+         }
+      },
+      [onSuccess, onError]
   );
   return (
-    <form onSubmit={sendContact}>
+   <form onSubmit={sendContact}>
       <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
+      <div className="col">
+         <Field placeholder="Votre Nom" label="Nom" />
+         <Field placeholder="Votre Prénom" label="Prénom" />
+         <Select
             selection={["Personel", "Entreprise"]}
             onChange={() => null}
             label="Personel / Entreprise"
             type="large"
             titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+         />
+         <Field placeholder="exemple@mail.com" label="Email" />
+         <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
             {sending ? "En cours" : "Envoyer"}
-          </Button>
-        </div>
-        <div className="col">
-          <Field
+         </Button>
+      </div>
+      <div className="col">
+         <Field
             placeholder="message"
             label="Message"
             type={FIELD_TYPES.TEXTAREA}
-          />
-        </div>
+         />
       </div>
-    </form>
+      </div>
+   </form>
   );
 };
 
