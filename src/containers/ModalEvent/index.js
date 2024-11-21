b4b91b5d8038429c5480dd5f5a48b660d5799569
Forcica <47173413+Forcica@users.noreply.github.com>
2024-11-21 14:49:52 +0100

    Update index.js

--- src/containers/ModalEvent/index.js
+++ src/containers/ModalEvent/index.js
@@ -3,34 +3,35 @@ import PropTypes from "prop-types";
 import "./style.scss";
 
 const ModalEvent = ({ event }) => (
-    <div className="ModalEvent">
+   <div className="ModalEvent">
       <div className="ModalEvent__imageContainer">
-        <img
-          data-testid="card-image-testid"
-          src={event.cover}
-          alt={event.title}
-        />
+         <img
+            src={`${process.env.PUBLIC_URL}${event.cover}`}
+            alt={event.title}
+         />
       </div>
       <div className="ModalEvent__title">
-        <div className="ModalEvent__titleLabel">{event.title}</div>
-        <div className="ModalEvent__titlePeriode">{event.periode}</div>
+         <div className="ModalEvent__titleLabel">{event.type}</div>
+         <h2>{event.title}</h2>
       </div>
       <div className="ModalEvent__descriptionContainer">
-        <h3>Description</h3>
-        <div>{event.description}</div>
+         <h3>Description</h3>
+         <div>{event.description}</div>
       </div>
       <div className="ModalEvent__descriptionContainer">
-        <h3>Participants</h3>
-        <div>{event.nb_guesses} participants</div>
+         <h3>Date</h3>
+         <div>{event.periode}</div>
       </div>
       <div className="ModalEvent__descriptionContainer">
-        <h3>Prestations</h3>
-        {event.prestations.map((presta) => (
-          <div key={presta}>{presta}</div>
-        ))}
+         <h3>Prestations</h3>
+         <div className="ModalEvent__prestationsList">
+            {event.prestations.map((presta) => (
+               <div key={presta}>{presta}</div>
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   </div>
+);
 
 ModalEvent.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
